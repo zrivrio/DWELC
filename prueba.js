530e416493e780b07a12fd2a9359d47150a1d954
zrivrio <zrivrio2103@gmail.com>
2024-09-20 09:35:08 +0200

    Inicio a javascript

---
+++ prueba.js
@@ -0,0 +1,16 @@
+let number = 6;
+console.log(number);
+
+number = 77;
+console.log(number);
+
+number = "hello everybody";
+console.log(typeof number); //muestra de que tipo es
+
+number = [1,2,3,"abc",false];
+console.log(number);
+
+let persona = {apellido:"ruiz"} ;
+console.log(persona);
+console.log(persona.hasOwnProperty("nombre")); //Muetra si una clase tiene alguno de estos tipos de propiedades
+console.log(persona.hasOwnProperty("apellido"));
