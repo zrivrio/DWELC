2fa8782715c8c8488205894c0babf0eaab903039
zrivrio <zrivrio2103@gmail.com>
2024-09-24 14:19:01 +0200

    Ejemplos de los strings

---
+++ Strings.js
@@ -0,0 +1,12 @@
+let persona = "Zahira";
+let city = "Malaga";
+let str = `hi ${persona}, are you in ${city}`;
+console.log(str);
+
+let str2 = `hi ${persona},
+ are you in:
+  ${city}`;
+  console.log(str2)
+
+let str3 = `hi ${persona}, are you in ${city}`;
+
