1f4f673492b555edbc0055d371963284c884c015
zrivrio <zrivrio2103@gmail.com>
2024-09-25 11:50:53 +0200

    ejercicio 3 javascript fundamentos

---
+++ Unit1/Ejercicios_jsfundamentos/ej3_jsfundamentos.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Primera Nota:")); 
+let b = parseInt(prompt("Segunda Nota:"));
+let c = parseInt(prompt("Tercera Nota:")); 
+let d = parseInt(prompt("Cuarta Nota:"));
+
+let media = (a,b,c,d) => a+b+c+d;
+media = media/4;
+
+
+
