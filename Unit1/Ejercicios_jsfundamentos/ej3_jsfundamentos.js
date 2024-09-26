79e7e120858994b41af3280505b2301e89bc1a16
zrivrio <zrivrio2103@gmail.com>
2024-09-26 10:07:55 +0200

    jercicio 3 de fundamentales de js terminado

--- Unit1/Ejercicios_jsfundamentos/ej3_jsfundamentos.js
+++ Unit1/Ejercicios_jsfundamentos/ej3_jsfundamentos.js
@@ -3,8 +3,17 @@ let b = parseInt(prompt("Segunda Nota:"));
 let c = parseInt(prompt("Tercera Nota:")); 
 let d = parseInt(prompt("Cuarta Nota:"));
 
-let media = (a,b,c,d) => a+b+c+d;
-media = media/4;
+let media = (a + b + c + d) / 4;
 
+let categoria;
+if (media < 5) {
+    categoria = "insuficiente";
+} else if (media >= 5 && media < 7) {
+    categoria = "suficiente";
+} else if (media >= 7 && media < 9) {
+    categoria = "notable";
+} else if (media >= 9 && media <= 10) {
+    categoria = "sobresaliente";
+}
 
-
+alert("Tu nota es " + media + ". Lo que equivale a un " + categoria);
