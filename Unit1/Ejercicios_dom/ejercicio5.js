ebbd50d24925769ce407dadff1eaead71732d467
zrivrio <zrivrio2103@gmail.com>
2024-10-23 09:40:29 +0200

    Correcciones

--- Unit1/Ejercicios_dom/ejercicio5.js
+++ Unit1/Ejercicios_dom/ejercicio5.js
@@ -1,16 +1,15 @@
-const colores = ["rgb(255,255,255)","rgb(0,255,0)","rgb(255,0,255)","rgb(255,0,0)"];
+const colores = ["rgb(0,0,255)","rgb(0,255,0)","rgb(255,0,255)","rgb(255,0,0)"];
 
 function coloresTabla(colores) {
     let celda = document.querySelectorAll("td");
     let contador = 0;
 
     for(const ele of celda){
-        if (contador < colores.length) {
-            ele.style.backgroundColor = colores[contador];
-            contador++;
-        }
+        if (contador == colores.length ) {
+            contador = 0;
+        } 
+        ele.style.backgroundColor = colores[contador];
+        contador++;
     }
-
 }
-
 coloresTabla(colores);
