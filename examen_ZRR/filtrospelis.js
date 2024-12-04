679ed0862dbafeaa3726e01c7aaf50e9456d4c85
zrivrio <zrivrio2103@gmail.com>
2024-12-04 11:50:18 +0100

    examen

---
+++ examen_ZRR/filtrospelis.js
@@ -0,0 +1,22 @@
+const countries = [
+    "USA",
+    "UK",
+    "Canada",
+    "Spain",
+    "France",
+    "Germany",
+    "Hong Kong",
+    "Ireland",
+];
+const genders = [
+    "Horror",
+    "Action",
+    "Fantasy",
+    "Drama",
+    "Sci-Fi",
+    "Biography",
+    "Comedy",
+    "Adventure",
+    "Thriller",
+    "History"
+];
