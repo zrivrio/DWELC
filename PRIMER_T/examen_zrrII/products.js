9e85f614dcca0dbfe15f1166adab09155ecd9930
zrivrio <zrivrio2103@gmail.com>
2024-12-13 10:23:15 +0100

    Examen 2

---
+++ PRIMER_T/examen_zrrII/products.js
@@ -0,0 +1,11 @@
+const products = [
+    { nombre: "Aloe 1", precio: 20.99, stock: 0, imagen: "./assets/img/aloevera835.jpg" },
+    { nombre: "Aloe 2", precio: 15.00, stock: 13, imagen: "./assets/img/aloevera834.jpg" },
+    { nombre: "Aloe 3", precio: 10.00, stock: 30, imagen: "./assets/img/aloevera829.jpg" },
+    { nombre: "Aloe 4", precio: 18.50, stock: 3, imagen: "./assets/img/aloevera826.jpg" },
+    { nombre: "Aloe 5", precio: 5.00, stock: 10, imagen: "./assets/img/aloevera825.jpg" },
+    { nombre: "Aloe 6", precio: 10.50, stock: 3, imagen: "./assets/img/aloevera816.jpg" },
+    { nombre: "Aloe 7", precio: 5.50, stock: 10, imagen: "./assets/img/aloevera807.jpg" }
+]
+
+   
