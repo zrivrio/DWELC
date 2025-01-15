ff62103cb885b3045d73a9117ac9861dc8383f13
zrivrio <zrivrio2103@gmail.com>
2025-01-15 13:40:52 +0100

    angular ejercicios

--- SEGUNDO_T/ANGULAR/ejercicios_Iniciales/src/app/navbar/navbar.component.ts
+++ SEGUNDO_T/ANGULAR/ejercicios_Iniciales/src/app/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
