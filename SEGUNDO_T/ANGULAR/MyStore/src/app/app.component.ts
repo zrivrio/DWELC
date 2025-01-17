21b31b8fb32f14806e184b0243868af18c3ae718
zrivrio <zrivrio2103@gmail.com>
2025-01-17 13:57:41 +0100

    Petardea no lo siguiente

--- SEGUNDO_T/ANGULAR/MyStore/src/app/app.component.ts
+++ SEGUNDO_T/ANGULAR/MyStore/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./product-list/product-list.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, TopBarComponent],
+  imports: [RouterOutlet, ProductListComponent, TopBarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- SEGUNDO_T/ANGULAR/MyStore/src/app/products.ts
+++ SEGUNDO_T/ANGULAR/MyStore/src/app/products.ts
@@ -1,4 +1,5 @@
 export type Product = {
     name:string;
     price:number;
+    description:string;
 }
