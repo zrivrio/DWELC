e360e2184b1b9ca9f19edab1f15321048cfbb5ac
zrivrio <zrivrio2103@gmail.com>
2025-01-17 14:14:11 +0100

    Petardea no lo siguiente

--- SEGUNDO_T/ANGULAR/MyStore/src/app/product-list/product-list.component.ts
+++ SEGUNDO_T/ANGULAR/MyStore/src/app/product-list/product-list.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
 import { Product } from '../products'; 
 
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule],
+  imports: [CommonModule,ProductAlertsComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
   products = [...products];
 
   share() {
