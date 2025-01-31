279011455099cb19817145b44597a365c9cbd858
zrivrio <zrivrio2103@gmail.com>
2025-01-31 09:20:00 +0100

    se ve la info de cada casa en particular

--- SEGUNDO_T/ANGULAR/home-app/src/app/app.component.ts
+++ SEGUNDO_T/ANGULAR/home-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- SEGUNDO_T/ANGULAR/home-app/src/app/housing-location/housing-location.component.ts
+++ SEGUNDO_T/ANGULAR/home-app/src/app/housing-location/housing-location.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
-import { Housinglocation } from '../housinglocation';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Housinglocation} from '../housinglocation';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
