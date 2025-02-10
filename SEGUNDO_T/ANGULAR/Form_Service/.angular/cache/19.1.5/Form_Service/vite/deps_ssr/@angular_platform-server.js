4411152d74b1ff2b51eecad65f9281ac3d5ef2ec
zrivrio <zrivrio2103@gmail.com>
2025-02-10 11:09:18 +0100

    nav con enlaces

---
+++ SEGUNDO_T/ANGULAR/Form_Service/.angular/cache/19.1.5/Form_Service/vite/deps_ssr/@angular_platform-server.js
@@ -0,0 +1,35 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BEFORE_APP_SERIALIZED,
+  ENABLE_DOM_EMULATION,
+  INITIAL_CONFIG,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS,
+  PlatformState,
+  SERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule
+} from "./chunk-HZ3QKO2S.js";
+import "./chunk-XW7SY7VP.js";
+import "./chunk-YAGPW5AS.js";
+import "./chunk-53Z7NWHP.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  BEFORE_APP_SERIALIZED,
+  INITIAL_CONFIG,
+  PlatformState,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule,
+  ENABLE_DOM_EMULATION as ɵENABLE_DOM_EMULATION,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS,
+  SERVER_CONTEXT as ɵSERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS
+};
--- SEGUNDO_T/ANGULAR/app-event/src/app/components/navbar/navbar.component.ts
+++ SEGUNDO_T/ANGULAR/app-event/src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeM } from '../../models/employeeM.model';
 import { EmployeeSService } from '../../services/employee-s.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
