49535578084c2f399a53391b46a4346783da07b2
Zahira <zrivrio2103@gmail.com>
2025-02-11 19:39:02 +0100

    intento que se muestre la lista

--- SEGUNDO_T/ANGULAR/BeatDrop/src/app/componets/Albumes/listar-albumes/listar-albumes.component.ts
+++ SEGUNDO_T/ANGULAR/BeatDrop/src/app/componets/Albumes/listar-albumes/listar-albumes.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { FooterComponent } from '../../footer/footer.component';
+import { Album } from '../../../models/albumes';
+import { AlbumesService } from '../../../services/albumes.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-listar-albumes',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CommonModule, RouterModule],
   templateUrl: './listar-albumes.component.html',
   styleUrl: './listar-albumes.component.css'
 })
-export class ListarAlbumesComponent {
+export class ListarAlbumesComponent implements OnInit {
+  albumes: Album[] = [];
+
+  constructor(private albumesService: AlbumesService) {}
+
+  ngOnInit(): void {
+    this.albumesService.getAlbumes().subscribe(data => {
+      this.albumes = data;
+    });
+  }
 
 }
--- SEGUNDO_T/ANGULAR/BeatDrop/src/app/services/albumes.service.ts
+++ SEGUNDO_T/ANGULAR/BeatDrop/src/app/services/albumes.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Album } from '../models/albumes';
+import { Album } from '../models/albumes'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumesService {
-  private listaAlbumes : Album[] = [];
+  private albumesSubject = new BehaviorSubject<Album[]>([]);
 
-  constructor() { 
+  constructor() {
     this.loadAlbumes();
   }
 
+  
   getAlbumes() {
-    return this.listaAlbumes;
+    return this.albumesSubject.asObservable();
   }
 
-  addAlbumes(album : Album): void{
-    console.log("se añade");
-    album.id = this.listaAlbumes.length + 1;
-
-    this.listaAlbumes.push(album);
+  addAlbum(album: Album): void {
+    const albumesActuales = this.albumesSubject.getValue();
+    album.id = albumesActuales.length + 1;
+    const nuevosAlbumes = [...albumesActuales, album];
+    this.albumesSubject.next(nuevosAlbumes);
   }
 
-   private loadAlbumes() {
-    fetch('http://localhost:3000/albumes')
-      .then(response => response.json())
-      .then(data => {
-        this.listaAlbumes.push(data); 
-      })
-      .catch(error => console.error('Error al cargar los empleados:', error));
+  private async loadAlbumes() {
+    try {
+      const response = await fetch('http://localhost:3000/albumes');
+      const data: Album[] = await response.json();
+      this.albumesSubject.next(data);
+    } catch (error) {
+      console.error('Error al cargar los álbumes:', error);
+    }
   }
 }
