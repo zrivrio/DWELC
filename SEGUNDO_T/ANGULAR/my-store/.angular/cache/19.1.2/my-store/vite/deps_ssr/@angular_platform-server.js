5fb76ca1848e2aaf15ec96955e418e951e415b17
zrivrio <zrivrio2103@gmail.com>
2025-02-14 08:29:04 +0100

    telefonos

---
+++ SEGUNDO_T/ANGULAR/my-store/.angular/cache/19.1.2/my-store/vite/deps_ssr/@angular_platform-server.js
@@ -0,0 +1,36 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BEFORE_APP_SERIALIZED,
+  ENABLE_DOM_EMULATION,
+  INITIAL_CONFIG,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS,
+  PlatformState,
+  SERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule
+} from "./chunk-3KKK6ENQ.js";
+import "./chunk-NVCAFEZK.js";
+import "./chunk-RBTN2UGQ.js";
+import "./chunk-6QKLVJN3.js";
+import "./chunk-2EWNKAHN.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  BEFORE_APP_SERIALIZED,
+  INITIAL_CONFIG,
+  PlatformState,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule,
+  ENABLE_DOM_EMULATION as ɵENABLE_DOM_EMULATION,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS,
+  SERVER_CONTEXT as ɵSERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS
+};
---
+++ SEGUNDO_T/ANGULAR/my-store/.angular/cache/19.1.2/my-store/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,36 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-K7CC7EGS.js";
+import "./chunk-2EIFDIRF.js";
+import "./chunk-3KKK6ENQ.js";
+import "./chunk-NVCAFEZK.js";
+import "./chunk-RBTN2UGQ.js";
+import "./chunk-6QKLVJN3.js";
+import "./chunk-2EWNKAHN.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
